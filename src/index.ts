77ec8ec0a5ae3661be3aab0c9aba19a870794895
Mats Hesmann <huesmann.mats@gmail.com>
2022-07-22 13:13:41 +0200

    System Tray test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import * as NodeGUI from '@nodegui/nodegui';
 
+NodeGUI.QApplication.instance().setQuitOnLastWindowClosed(false);
+
 const window = new NodeGUI.QMainWindow();
+window.setWindowTitle("NodeGUI Demo");
+window.resize(400, 700);
+window.show();
 const trayIcon = new NodeGUI.QIcon(`C:/Users/User/Pictures/Terraconnect256x256.png`);
 const tray = new NodeGUI.QSystemTrayIcon();
 tray.setIcon(trayIcon);
