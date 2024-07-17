f73eb975874c2e485971a7265027478abb8674f4
Mats Huesmann <huesmann.mats@gmail.com>
2024-07-17 20:16:34 +0200

    Refactor JSX

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,43 @@ import { ChildModified, Modified, PreserveState, State, Value, createState, isSt
 export { MouseEventHandler };
 
 export type stateify<T> = { [K in keyof T]: State<T[K]> | T[K] };
-export type children = State<HTMLComponent<unknown> | Array<HTMLComponent<unknown> | undefined | string> | State<HTMLComponent<unknown> | undefined | string | Array<Element | undefined | string>> | undefined | string> | stateify<HTMLComponent<unknown> | Array<HTMLComponent<unknown> | undefined | string>> | undefined | string;
+// export type children = State<HTMLComponent<unknown> | Array<HTMLComponent<unknown> | undefined | string> | State<HTMLComponent<unknown> | undefined | string | Array<Element | undefined | string>> | undefined | string> | stateify<HTMLComponent<unknown> | Array<HTMLComponent<unknown> | undefined | string>> | undefined | string;
+export type children = State<HTMLComponent<any> | Array<HTMLComponent<any> | undefined | string> | undefined | string> | HTMLComponent<any> | Array<HTMLComponent<any> | undefined | string> | undefined | string;
 export type props<T> = stateify<T> & { children?: children };
 
-export type Component<T = {}, S = T & { [data: `data-${string}`]: any }> = (this: HTMLComponent<T>, props: State<S | { children: Array<HTMLComponent<unknown>> }> | S | { children: Array<HTMLComponent<unknown>> }) => stateify<HTMLComponent<unknown> | Array<HTMLComponent<unknown>>>;
-export type ComponentFN<T = {}> = (this: HTMLComponent<T>, props: State<T & { children: Array<HTMLComponent<unknown>> }>) => stateify<HTMLComponent<unknown> | Array<HTMLComponent<unknown>>>;
-export type HTMLComponent<T> = HTMLElement & { component: string | Component<any>, props: State<HTMLComponentProps<T>> };
+export type Component<T = {}, S = T & { [data: `data-${string}`]: any }> = (this: HTMLComponent<T>, props: State<S | { children: Array<HTMLComponent<unknown>> }> | S | { children: Array<HTMLComponent<unknown>> }) => children;
+export type ComponentFN<T = {}> = (this: HTMLComponent<T>, props: State<T & { children?: Array<HTMLComponent<unknown>> }>) => children;
+export type HTMLComponent<T> = HTMLElement & { component: string | Component<any>, props: State<HTMLComponentProps<T>>, cchildren: State<(Text | HTMLComponent<T> | null)[]> };
 export type HTMLComponentProps<T> = T & { children: Array<HTMLComponent<unknown>> };
 
 const SVGElements = ["svg", "animate", "animateMotion", "animateTransform", "circle", "clipPath", "defs", "desc", "ellipse", "feBlend", "feColorMatrix", "feComponentTransfer", "feComposite", "feConvolveMatrix", "feDiffuseLighting", "feDisplacementMap", "feDistantLight", "feDropShadow", "feFlood", "feFuncA", "feFuncB", "feFuncG", "feFuncR", "feGaussianBlur", "feImage", "feMerge", "feMergeNode", "feMorphology", "feOffset", "fePointLight", "feSpecularLighting", "feSpotLight", "feTile", "feTurbulence", "filter", "foreignObject", "g", "image", "line", "linearGradient", "marker", "mask", "metadata", "mpath", "path", "pattern", "polygon", "polyline", "radialGradient", "rect", "stop", "switch", "symbol", "text", "textPath", "tspan", "use", "view"];
 const HTMLElements = ["a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "keygen", "label", "legend", "li", "link", "main", "map", "mark", "menu", "menuitem", "meta", "meter", "nav", "noindex", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "slot", "script", "section", "select", "small", "source", "span", "strong", "style", "sub", "summary", "sup", "table", "template", "tbody", "td", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "u", "ul", "var", "video", "wbr", "webview"];
 
-export function createElement(type: string, props: any) {
+(window as any).objects = [];
+
+const camelToSnakeCase = (s: string) => s.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`);
+
+export function createComponentElement<T>(component: string | Component<any>, props: props<T>): HTMLComponent<T> {
+  let tagName = "app" + camelToSnakeCase((component as any).name);
+  tagName = tagName != "app-app" ? tagName : "app-root"
+  let element = document.createElement(tagName) as HTMLComponent<T>;
+  element.component = component;
+  // element.cchildren = createState([] as Array<any>);
+  applyPropsAsAtributes(element, props);
+  return element;
+}
+
+export function formatComponentProps<T>(props: props<T>): props<T> {
+  props.children = formatChildren(props.children) as children;
+  return props;
+}
+
+export function callComponent<T>(element: HTMLComponent<T>) {
+  if (typeof element.component == "string") return;
+  return element.component.bind(element)(element.props as Parameters<Component>[0])
+}
+
+export function createElement<T>(type: string, props: any) {
   const applyProps = (element: HTMLElement | SVGElement) => {
     Object.keys(props).filter(key => props[key] != null).forEach(key => {
       if (props[key][Value] == null) {
@@ -72,11 +97,11 @@ export function createElement(type: string, props: any) {
   if (SVGElements.includes(type)) {
     let element = document.createElementNS("http://www.w3.org/2000/svg", type);
     applyProps(element);
-    return element;
+    return element as Element as HTMLComponent<T>;
   } else if (HTMLElements.includes(type)) {
     let element = document.createElementNS("http://www.w3.org/1999/xhtml", type);
     applyProps(element);
-    return element;
+    return element as HTMLComponent<T>;
   }
   throw new Error("Intrinsic Element not found!");
 }
@@ -101,7 +126,9 @@ export function applyPropsAsAtributes<T>(element: HTMLComponent<T>, props: props
         data = value.toString();
         break;
       case "object":
-        data = JSON.stringify(value);
+        let i = ((window as any).objects as Array<any>).findIndex(_ => _ == value);
+        if (i == -1) i = ((window as any).objects as Array<any>).push(value) - 1;
+        data = `[${Array.isArray(value) ? "Array" : "Object"} ${i}]`// JSON.stringify(value);
         break;
       case "symbol":
         data = `[Symbol ${value.description}]`;
@@ -110,10 +137,12 @@ export function applyPropsAsAtributes<T>(element: HTMLComponent<T>, props: props
         data = `[Function ${value.name}]`;
         break;
     }
-    element?.setAttribute(key.replaceAll('on', 'on-'), data);
+    if (key.startsWith('on'))
+      key = `on-${key.substring(2)}`
+    element?.setAttribute(key, data);
   }
   Object.entries<State<unknown>>(attributes).forEach(([key, state]) => setAttribute(key, state[Value]));
-  attributes[ChildModified].on((newValue, key) => setAttribute((typeof key == "string") ? key : null, newValue));
+  attributes[ChildModified].on((newValue, key) => setAttribute((typeof key == "string" && !key.includes('.')) ? key : null, newValue));
   var observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
       if (mutation.type === "attributes" && mutation.attributeName) {
@@ -129,10 +158,10 @@ export function applyPropsAsAtributes<T>(element: HTMLComponent<T>, props: props
           prop[Value] = value === 'true';
         else if (value !== "" && !Number.isNaN(Number(value)))
           prop[Value] = Number(value);
-        else if (value.startsWith("[Symbol") || value.startsWith("[Symbol"))
+        else if (value.startsWith("[Symbol") || value.startsWith("[Function") || value.startsWith("[Object") || value.startsWith("[Array"))
           return;
-        else if ((value.startsWith("[") && value.endsWith("]")) || (value.startsWith("{") && value.endsWith("}")))
-          prop[Value] = JSON.parse(value);
+        // else if ((value.startsWith("[") && value.endsWith("]")) || (value.startsWith("{") && value.endsWith("}")))
+        //   prop[Value] = JSON.parse(value);
         else
           prop[Value] = value;
       }
@@ -143,49 +172,79 @@ export function applyPropsAsAtributes<T>(element: HTMLComponent<T>, props: props
   element.props[PreserveState] = true;
 }
 
-export function appendChildren<T>(element: HTMLComponent<T>, children: State<Array<HTMLComponent<T> | Text | null>>) {
+export function parseTextNode<T>(children: State<Array<HTMLComponent<T> | string | null>>) {
+  return children.map[Value](({ [Value]: child }) =>
+    child == null ? null :
+      child instanceof Node ? child :
+        document.createTextNode(child.toString())
+  ) as State<Array<HTMLComponent<T> | Text | null>>;
+}
+
+export function appendChildren<T>(element: HTMLComponent<T>, children: State<Array<HTMLComponent<T> | Text | null>>): HTMLComponent<T> {
   children.forEach[Value](({ [Value]: child }) => {
     if (child == null) return;
     element.append(child);
-    child.dispatchEvent(new Event('mount'));
+    child.dispatchEvent(new Event('mount', { bubbles: true }));
   });
-  children.forEach[Value](child => {
-    child[Modified].on((newValue, oldValue) => {
-      if (oldValue != null) {
-        oldValue.remove();
-        oldValue.dispatchEvent(new Event('unmount'));
-      }
-      if (newValue != null) {
-        element.append(newValue);
-        newValue.dispatchEvent(new Event('mount'));
-      }
-    });
-  });
+  // children.forEach[Value](child => {
+  //   child[Modified].on((newValue, oldValue) => {
+  //     if (oldValue != null) {
+  //       oldValue.remove();
+  //       oldValue.dispatchEvent(new Event('unmount'));
+  //     }
+  //     if (newValue != null) {
+  //       element.append(newValue);
+  //       newValue.dispatchEvent(new Event('mount'));
+  //     }
+  //   });
+  // });
   children[ChildModified].on((newValue, key, oldValue) => {
     if (typeof key == "string" && key.includes('.')) return;
     if (oldValue != null) {
       oldValue.remove();
-      oldValue.dispatchEvent(new Event('unmount'));
+      oldValue.dispatchEvent(new Event('unmount', { bubbles: true }));
     }
     if (newValue != null) {
+      if ((newValue as any)[Value] != null)
+        debugger;
       element.append(newValue);
-      newValue.dispatchEvent(new Event('mount'));
+      newValue.dispatchEvent(new Event('mount', { bubbles: true }));
     }
   });
+  // if (element.cchildren == null)
+  //   element.cchildren = createState([] as Array<any>);
+  // element.cchildren[Value] = [children] as any;
+  return element;
 }
 
-export function formatChildren<T>(children: children): State<Array<HTMLComponent<T> | Text | null>> {
+export function formatChildren<T>(children: children): State<Array<HTMLComponent<T> | string | null>> {
   if (children != null) {
     if (isState(children)) {
       if (children[Value] == null)
-        children[Value] = [] as unknown as undefined;
+        children[Value] = [] as any;
       else if (!Array.isArray(children[Value]))
-        children = createState<children>([children] as children) as children;
+        children = createState([children]) as any;
     } else if (!Array.isArray(children))
-      children = createState<children>([children] as children) as children;
+      children = createState([children]) as any;
     else
-      children = createState<children>(children) as children;
+      children = createState(children);
   } else
-    children = createState<children>([]) as children;
-  return (children as State<Array<HTMLComponent<T> | string | null>>).map[Value]<Array<HTMLComponent<T> | Text | null>>(({ [Value]: child }) => child == null ? null : child instanceof Node ? child : document.createTextNode(child.toString()));
+    children = createState([]) as any;
+  return children as State<Array<HTMLComponent<T> | string | null>>;
 }
+
+// export function formatChildren<T>(children: children): State<Array<HTMLComponent<T> | string | null>> {
+//   if (children != null) {
+//     if (isState(children)) {
+//       if (children[Value] == null)
+//         children[Value] = [] as unknown as undefined;
+//       else if (!Array.isArray(children[Value]))
+//         children = createState<children>([children] as children) as children;
+//     } else if (!Array.isArray(children))
+//       children = createState<children>([children] as children) as children;
+//     else
+//       children = createState<children>(children) as children;
+//   } else
+//     children = createState<children>([]) as children;
+//   return children as State<Array<HTMLComponent<T> | string | null>>;
+// }
--- src/jsx-runtime.ts
+++ src/jsx-runtime.ts
@@ -1,27 +1,15 @@
 import './dom';
-import { Component, HTMLComponent, children, props, formatChildren, createElement, appendChildren, applyPropsAsAtributes } from '.';
-
-const camelToSnakeCase = (s: string) => s.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`);
+import { Component, children, props, formatChildren, createElement, appendChildren, parseTextNode, createComponentElement, callComponent, formatComponentProps } from '.';
 
 export function jsx<T>(component: typeof Fragment | string | Component<any>, props: props<T>): children {
   if (component == Fragment)
     return props.children;
   if (typeof component === 'function') {
-    let tagName = "app" + camelToSnakeCase((component as any).name);
-    tagName = tagName != "app-app" ? tagName : "app-root"
-    let element = document.createElement(tagName) as HTMLComponent<T>;
-    element.component = component;
-    props.children = formatChildren(props.children) as children;
-    applyPropsAsAtributes(element, props);
-    let children = formatChildren(component.bind(element)(props as unknown as Parameters<Component>[0]));
-    appendChildren(element, children);
-    return element;
+    let element = createComponentElement(component, formatComponentProps(props));
+    return appendChildren(element, parseTextNode(formatChildren(callComponent(element))));
   } else {
-    let children = formatChildren(props.children);
-    delete (props.children);
-    let element = createElement(component, props) as HTMLComponent<T>;
-    appendChildren(element, children);
-    return element;
+    let { children, ...args } = props;
+    return appendChildren(createElement(component, args), parseTextNode(formatChildren(children)));
   }
 }
 export const jsxs = jsx;
